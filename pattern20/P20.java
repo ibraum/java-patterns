3fdf6d53cc3bb80ece84edb1d2ea055e45e8b02d
ibraum <ibraumodnok@gmail.com>
2025-01-28 22:03:51 +0000

    p20

---
+++ pattern20/P20.java
@@ -0,0 +1,10 @@
+public class P20 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                
+            }
+        }
+    }
+}
