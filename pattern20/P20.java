87c16e5b04df969c222ab3783a0eb9f43b7c6c97
ibraum <ibraumodnok@gmail.com>
2025-02-10 23:34:03 +0000

    pattern20

--- pattern20/P20.java
+++ pattern20/P20.java
@@ -3,8 +3,15 @@ public class P20 {
         int n = 5;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                
+                if (i % 2 != 0) {
+                    System.out.print("0");
+                } else if((i+j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
             }
+            System.out.println();
         }
     }
 }
