9b5c184d5cb7661d52886f0789aace00c05cc615
ibraum <ibraumodnok@gmail.com>
2025-02-16 22:10:28 +0000

    pattern31

--- pattern31/P31.java
+++ pattern31/P31.java
@@ -1,5 +1,12 @@
 public class P31 {
     public static void main(String[] args) {
+        char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
         
+        for (int i = 0; i < alphabet.length; i++) {
+            for (int j = i; j < i + 6 && j == alphabet.length - 1; j++) {
+                System.out.print(alphabet[j]);
+            }
+            System.out.println();
+        }
     }
 }
